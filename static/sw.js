1ad1949c2fa7ffd50226941faa7dffc767e5fe08
benamedd <benseti@yahoo.fr>
2025-03-31 15:52:08 +0100

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,22 +1,7 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('leafscan-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/static/style.css',
-                '/static/script.js',
-                '/static/manifest.json',
-                '/static/icons/icon-192x192.png',
-                '/static/icons/icon-512x512.png'
-            ]);
-        })
-    );
+  console.log('Service Worker installed');
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(fetch(event.request));
 });
